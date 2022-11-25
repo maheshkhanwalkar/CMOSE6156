abe99aa5edb66d28d3b9908cba2a2e130b53c7cc
Mahesh Khanwalkar <maheshkhanwalkar@gmail.com>
2022-11-24 20:27:50 -0500

    Add CORS filter to support OPTIONS preflight request

---
+++ Post/src/main/java/com/Post/config/WebSecurityConfig.java
@@ -0,0 +1,21 @@
+package com.Post.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig
+{
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class);
+        httpSecurity.csrf().disable();
+
+        return httpSecurity.build();
+    }
+}
